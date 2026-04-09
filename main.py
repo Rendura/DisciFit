dd8aac6a11797f26dba31c1d69cd2ec37180568b
Rei <ceanandreir@gmail.com>
2026-04-09 19:22:14 +0800

    feat: add basic user input for weight and height tracking

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig, ax = plt.subplots()             # Create a figure containing a single Axes.
-ax.plot([1, 2, 3, 4], [1, 4, 2, 3])  # Plot some data on the Axes.
-plt.show()                           # Show the figure.
+class User:
+    def initialize(self, input_desire_weight, input_duration):
+        weight_goal_kg = input_desire_weight
+        goal_duration = input_duration
+
+        return weight_goal_kg, goal_duration
+
+    def info(self, user_height, user_weight):
+        height = user_height
+        weight = user_weight
+        
+        return height, weight
+    
+
+target_weight_data = input("Set Your Target Weight (KG): ")
+target_duration_data = input("Enter Your Target Timeframe (Weeks): ")
+
+user_height_cm = input("Enter your height in cm: ")
+user_weight_kg = input("Enter your current weight in kg: ")
+
+user = User()
+print(user.initialize(target_weight_data, target_duration_data))
+print(user.info(user_height_cm, user_weight_kg))
